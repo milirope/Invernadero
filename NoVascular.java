801df30b268fd5abced1dacd2df14f538e767fd2
milirope <64318749+milirope@users.noreply.github.com>
2021-10-30 12:34:10 -0400

    Create NoVascular.java

---
+++ NoVascular.java
@@ -0,0 +1,8 @@
+
+package com.mycompany.invernadero;
+
+public interface NoVascular {
+    
+    String procesoNoVascularInfo = "Una planta no vascular​ es aquella que carece de sistema vascular (tallo y flor)";
+    String mostrarProcesoNoVascular();
+}
