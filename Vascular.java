96d0eed54d5894efed7557ea98d2f7774e82bdcc
milirope <64318749+milirope@users.noreply.github.com>
2021-10-30 12:35:16 -0400

    Create Vascular.java

---
+++ Vascular.java
@@ -0,0 +1,10 @@
+
+package com.mycompany.invernadero;
+
+
+public interface Vascular {
+    String procesoVascularInfo = "El tejido vascular de las plantas distribuye agua y nutrientes, "+
+                                   "lo que garantiza un crecimiento constante.";
+    
+    String mostrarProcesoVascular();
+}
